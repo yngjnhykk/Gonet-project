057c2a803c61cbf1da3862c6e667733ac26f33f8
jinhyeok99 <143480840+yngjnhykk@users.noreply.github.com>
2024-04-20 12:02:04 +0900

    feat: project CRUD

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/main/MainPage';
 import RegisterPage from './pages/register/RegisterPage';
+import EditPage from './pages/edit/EditPage';
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/register' element={<RegisterPage />} />
+        <Route path='/edit' element={<EditPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const apiUrl = 'http://localhost:3001';
 
+// GET - 부서별 프로젝트 전체 조회
+
 export const getDatas = async (department) => {
   try {
     const response = await axios.get(`${apiUrl}/${department}`);
@@ -11,14 +13,17 @@ export const getDatas = async (department) => {
   }
 };
 
+// POST - Project 생성
+
 export const postProject = async (department, newProject) => {
   console.log(department, newProject);
   try {
-    // new projectCode 생성
+    // new Code 생성
     await axios.get(`${apiUrl}/${department}`).then((response) => {
       const data = response.data;
       const projects = data.projects;
 
+      // 부서 내 가장 최근 프로젝트
       const recentProject = projects[0].projectCode;
 
       const newCode =
@@ -27,32 +32,99 @@ export const postProject = async (department, newProject) => {
         (parseInt(recentProject.substring(recentProject.indexOf('-') + 1)) + 1).toString();
 
       // new Key 생성
+      axios.get(`${apiUrl}/3`).then((response) => {
+        const recentKey = response.data.recentKey;
+
+        axios
+          .patch(`${apiUrl}/3`, { recentKey: recentKey + 1 })
+          .then((response) => {
+            const newKey = response.data.recentKey;
+
+            const updateProject = {
+              ...newProject,
+              key: newKey,
+              projectCode: newCode,
+            };
+
+            projects.unshift(updateProject);
+
+            // new Project 생성
+            axios
+              .put(`${apiUrl}/${department}`, data)
+              .then((updatedResponse) => {
+                console.log('새로운 프로젝트가 성공적으로 추가되었습니다.');
+                console.log(updatedResponse);
+              })
+              .catch((err) => {
+                console.error('데이터를 가져오는 중 오류가 발생했습니다.', err);
+              });
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      });
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Post - 프로젝트 수정
+
+export const editProject = async (department, newProject) => {
+  try {
+    await axios.get(`${apiUrl}/${department}`).then((response) => {
+      const data = response.data;
+      const projects = data.projects;
+
+      const newProjects = projects.map((project) => {
+        if (project.key === newProject.key) {
+          return newProject;
+        }
+        return project;
+      });
+
+      const newData = { ...data, projects: newProjects };
+
       axios
-        .get(`${apiUrl}/3`)
-        .then((response) => {
-          const newKey = response.data.recentKey + 1;
-
-          const updateProject = {
-            ...newProject,
-            key: newKey,
-            projectCode: newCode,
-          };
-
-          projects.unshift(updateProject);
-
-          // new Project 생성
-          axios
-            .put(`${apiUrl}/${department}`, data)
-            .then((updatedResponse) => {
-              console.log('새로운 프로젝트가 성공적으로 추가되었습니다.');
-              console.log(updatedResponse);
-            })
-            .catch((err) => {
-              console.error('데이터를 가져오는 중 오류가 발생했습니다.', err);
-            });
+        .put(`${apiUrl}/${department}`, newData)
+        .then((updatedResponse) => {
+          console.log('프로젝트가 성공적으로 수정되었습니다.');
+          console.log(updatedResponse);
         })
         .catch((err) => {
-          console.error(err);
+          console.error('데이터를 수정하는 중 오류가 발생했습니다.', err);
+        });
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// POST - 프로젝트 삭제
+
+export const deleteProject = async (department, projectKey) => {
+  try {
+    await axios.get(`${apiUrl}/${department}`).then((response) => {
+      const data = response.data;
+      const projects = data.projects;
+
+      const newProjects = projects.filter((project) => {
+        return project.key !== projectKey;
+      });
+
+      console.log(newProjects);
+
+      const newData = { ...data, projects: newProjects };
+
+      axios
+        .put(`${apiUrl}/${department}`, newData)
+        .then((updatedResponse) => {
+          console.log('프로젝트가 성공적으로 삭제되었습니다.');
+          console.log(updatedResponse);
+        })
+        .catch((err) => {
+          console.error('데이터를 삭제하는 중 오류가 발생했습니다.', err);
         });
     });
   } catch (err) {
